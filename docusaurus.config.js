f687c25027dddea000fd849f360ab0b6ed853c8e
Ajay Prem Shankar <ajaypremshankar@gmail.com>
2024-01-20 21:23:05 +0530

    Updated docu config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,19 +9,19 @@ import { themes as prismThemes } from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Ubella Maria',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'A Teacher and Student for life.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ubellah.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ubellah', // Usually your GitHub org/user name.
+  projectName: 'ubellah.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
